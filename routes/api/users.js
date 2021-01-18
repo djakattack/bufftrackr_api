aa62562bf9f59d098dd8bc5c58566b2a734aa694
jpearce2014 <jlpearce.phd@gmail.com>
2021-01-18 16:53:29 -0500

    Can now add users to the database, hash their passwords.  Verification steps added to ensure no duplicate users or emails, and to see if the user is already registered.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator')
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+
+const User = require('../../models/User');
 
 // @route   POST api/users
 // @descr   Register User
@@ -12,13 +15,43 @@ router.post(
         check('email', 'Please include a valid email address').isEmail(),
         check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
     ],
-    (req, res) => {
+    async (req, res) => {
         // console.log(req.body);
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        res.send(`User Created`);
+        const { name, email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+            if (user) {
+                res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+            }
+            user = new User({
+                name,
+                email,
+                password
+            });
+            console.log(`User: ${name}`);
+            console.log(`Email: ${email}`);
+            console.log(`password (pre-hash): ${password}`);
+
+            //Create a salt to do the password hashing
+            const salt = await bcrypt.genSalt(10);
+            console.log(`salt: ${salt}`);
+            user.password = await bcrypt.hash(password, salt);
+            console.log(`password (post hash): ${password}`);
+            user.save();
+
+            // Return JSON Web Token
+            res.send(`User Created`);
+        } catch (err) {
+            console.error(gradient.mind(err.message));
+            res.status(500).send('Server Error');
+            process.exit(1);
+        }
+
     }
 );
 
