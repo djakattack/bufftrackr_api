a575975336bea60e30ebb1bd912754da4ac5cb2e
jpearce2014 <jlpearce.phd@gmail.com>
2021-01-22 15:54:18 -0500

    Can now create and delete comments.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -86,10 +86,41 @@ router.post(
     }
 );
 
-// @route   POST api/posts
+// @route   POST api/posts/comment/:id
 // @descr   Comment on a post
 // @access  Private
-router.post('/', auth, async (req, res) => { });
+router.post(
+    '/comment/:id',
+    [
+        auth,
+        [
+            check('text', 'Text is required').not().isEmpty()
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        try {
+            const user = await User.findById(req.user.id).select('-password');
+            const post = await Post.findById(req.params.id);
+            const newComment = {
+                text: req.body.text,
+                name: user.name,
+                user: req.user.id
+            }
+
+            post.comments.unshift(newComment);
+
+            await post.save();
+            res.json(post.comments);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+    }
+);
 
 
 // ---------------
@@ -97,13 +128,41 @@ router.post('/', auth, async (req, res) => { });
 // ---------------
 
 
-// @route   DELETE api/posts
-// @descr   Delete as comment
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @descr   Delete a comment
 // @access  Private
-router.delete('/', auth, async (req, res) => { });
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // Pull out comment
+        const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+        // Make sure comment exits
+        if (!comment) {
+            return res.status(404).json({ msg: 'Comment does not exist' });
+        }
+        // Check user
+        if (comment.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: "User not authorized" })
+        }
+
+        // Get remove index
+        const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+        post.comments.splice(removeIndex, 1);
+
+        await post.save();
+        res.json(post.comments);
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind == 'ObjectId') {
+            return res.status(400).json({ msg: 'comment Not Found' })
+        }
+        res.status(500).send('Server Error');
+    }
+});
 
 
-// @route   DELETE api/posts
+// @route   DELETE api/posts/:id
 // @descr   Delete a post
 // @access  Private
 router.delete('/:id', auth, async (req, res) => {
@@ -184,6 +243,14 @@ router.put('/unlike/:id', auth, async (req, res) => {
     }
 });
 
+// @todo    These two routes are not part of the tutorial, but I think it'd be nice to have.
 
+// @route   PUT api/posts/like/comment/:id
+// @descr   Like a comment
+// @access  Private
+
+// @route   PUT api/posts/ulike/comment/:id
+// @descr   Unike a comment
+// @access  Private
 
 module.exports = router;
