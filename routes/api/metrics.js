4cc0e385f55328a16774b3d63334f1bed0cc2577
jpearce2014 <jlpearce.phd@gmail.com>
2021-01-24 09:04:35 -0500

    Metrics route test successful.

--- routes/api/metrics.js
+++ routes/api/metrics.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../../middleware/auth');
-const { check, validationResult } = require('express-validator');
-const Metrics = require('../../models/Metrics');
 
-// ------------
-// GET REQUESTS
-// ------------
+router.get('/', (req, res) => res.send('Test route'));
 
 module.exports = router;
